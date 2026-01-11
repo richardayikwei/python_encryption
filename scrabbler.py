af1d0f5cccbf1c246a9eccd06a168d4992027633
Richard Ayikwei <richardayikweibee@gmail.com>
2026-01-11 14:40:49 +0000

    refactor(scrabbler.py): handles error if cypher execeeds characer range

--- scrabbler.py
+++ scrabbler.py
@@ -4,12 +4,9 @@ def scrabbeler(cypher:int,path:str) -> str:
     '''
     Encrypt text using using cypher
     
-    cypher (int): Description
-    :type cypher: int
-    :param path: Description
-    :type path: str
-    :return: Description
-    :rtype: str
+    cypher (int): Possitive non zero integer
+    path (str): Path to file 
+    return (str) : write a text document
     '''
     if cypher == 0:
         print('No encryption applied. Exiting program...............')
@@ -38,12 +35,23 @@ def scrabbeler(cypher:int,path:str) -> str:
 
 
 def unscrabbeler(cypher:int,path:str) -> str:
+    '''
+    Encrypt text using using cypher
+    
+    cypher (int): Possitive non zero integer
+    path (str): Path to file 
+    return (str) : write a text document
+    '''
 
     if cypher == 0:
         print('No decryption applied. Exiting program...............')
         return
+    elif cypher < 0:
+        print('Cypher must be a positive integer')
+        return
     
     cypher_digit = re.match(r'\d{1,5}$',str(cypher))
+
     try:
         cypher_digit.group()
         corr_num = True
@@ -51,14 +59,18 @@ def unscrabbeler(cypher:int,path:str) -> str:
         corr_num = False
         print(f'Cypher requires at least 1 Digit and not more than 5 digit\n{"=" * 58}',
               f'\nCurrent number of digits = {len(str(cypher))}')
+        
     if corr_num == True:
-        with open(path,'r',encoding='utf-8',) as begin:
-            file = begin.read()
-        scrabble = [chr(ord(i) - cypher) for i in file]
-        revealed = ''.join(scrabble)
-        with open('revealed.txt','w',encoding='utf-8') as f:
-            f.write(revealed)
+        try:
+            with open(path,'r',encoding='utf-8',) as begin:
+                file = begin.read()
+            scrabble = [chr(ord(i) - cypher) for i in file]
+            revealed = ''.join(scrabble)
+            with open('revealed.txt','w',encoding='utf-8') as f:
+                f.write(revealed)
+        except:
+            print('Decryption cycpher exceeded text character range')
 
 
-scrabbeler(-10000,'Power.txt')
-unscrabbeler(0,'hidden.txt')
+scrabbeler(998,'Power.txt')
+unscrabbeler(999,'hidden.txt')
